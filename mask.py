ff904ebabe95fba1d08be62f7f13365a705ab1e7
Richard Pinheiro <richardpinheiro1992@gmail.com>
2025-07-21 19:54:21 -0300

    Implement get_color_for_attention_score to map attention scores to grayscale

--- mask.py
+++ mask.py
@@ -57,8 +57,8 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    # TODO: Implement this function
-    raise NotImplementedError
+    value = int((1 - attention_score) * 255)
+    return (value, value, value)
 
 
 
